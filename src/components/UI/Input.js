67cae2f106d94941baacd5793db2613c032423e3
Aleksandr-PC <aleksandrsglagolevs1@gmail.com>
2023-05-21 20:58:11 +0300

    update

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props) => {
   return (
     <div className={styles.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       <input {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
