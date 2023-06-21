3f97649117f884a5e51e26934910b56ee47c02c9
Aleksandr-PC <aleksandrsglagolevs1@gmail.com>
2023-06-21 16:30:48 +0300

    update

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,23 +1,14 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import styles from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
-  const [isAmountValid, setIsAmountValid] = useState(true);
   const amountInputRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     const inputAmount = amountInputRef.current.value;
-    if (
-      inputAmount.trim().length === 0 ||
-      +inputAmount < 1 ||
-      +inputAmount > 10
-    ) {
-      setIsAmountValid(false);
-      return;
-    }
 
     props.onAddToCart(+inputAmount);
   };
@@ -29,13 +20,13 @@ const MealItemForm = (props) => {
         input={{
           id: props.id,
           type: "number",
-          min: "min",
+          min: "1",
+          max: "10",
           step: "1",
           defaultValue: "1",
         }}
       />
       <button>Add</button>
-      {!isAmountValid && <p>Please enter amount from 1 to 10</p>}
     </form>
   );
 };
